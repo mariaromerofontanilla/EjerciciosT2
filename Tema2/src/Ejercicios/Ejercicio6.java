c0e6c5db0e12b8431e9459ec973668229b022df0
mariaromerofontanilla <145675441+mariaromerofontanilla@users.noreply.github.com>
2023-10-06 14:41:12 +0200

    Ejerciciop2

--- Tema2/src/Ejercicios/Ejercicio6.java
+++ Tema2/src/Ejercicios/Ejercicio6.java
@@ -13,50 +13,49 @@ public class Ejercicio6 {
 		int b;
 		//variable c
 		int c;
-		//variable calculo
+		//variable calculos
+		//variable donde queda el discriminante
 		double calculo;
-		//variable de calculo1
-		double calculo1;
 		//variable de calculofinal1
-		double calculofinal1;
+		double x1;
 		//variable de calculofinal2
-		double calculofinal2;
+		double x2;
 		//crear Scanner
 		Scanner sc = new Scanner(System.in);
 		
 		//Se lee por pantalla de que introduzcas un numero
-		System.out.println("ntroduce el primer numero: ");
+		System.out.println("introduce el primer numero: ");
 		//Se Scannear la variable a
 		a = sc.nextInt();
 		
 		//Se lee por pantalla de que introduzcas un numero
-		System.out.println("ntroduce el segundo numero: ");
+		System.out.println("introduce el segundo numero: ");
 		//Se Scannea la variable b
 		b = sc.nextInt();
 		
 		//Se lee por pantalla de que introduzcas un numero
-		System.out.println("ntroduce el tercer numero: ");
+		System.out.println("introduce el tercer numero: ");
 		//Se Scannea la variable c
 		c = sc.nextInt();
 		
-		//calculos de las variables calculo variables calculo1, calculofinal1 y calculofinal2
+		//calculos de las variables calculo, calculofinal1 y calculofinal2
 		calculo = (Math.pow(b, 2)-4*a*c);
-		calculo1 = (Math.sqrt(calculo));
-		calculofinal1 = (-b +calculo1)/(2*a);
-		calculofinal2 = (-b -calculo1)/(2*a);
 		
 		//calculo del if de que multiplicas 2*a y es == 0
-		if (2*a == 0) {
+		if (a == 0) {
 			//Se lee por pantalla de que el calculo no se puede hacer
-			System.out.println("El calculo no se puede hacer");
+			x1 = (double) -c / b;
+			System.out.println("El resultado es: " + x1);
 		//calculo del else if de la variable calculo es menor igual que 0	
-		} else if (calculo <= 0) {
+		} else if (calculo < 0) {
 			//Se lee por pantaa de que el calculo no se puede hacer
 			System.out.println("El calculo no se puede hacer");	
 		//calculo del else
 		} else {
 			//Se lee por pantalla de que la variable calculofinal1 y calculofinal2
-			System.out.println(calculofinal1 + " y " + calculofinal2);
+			x1 = (-b + Math.sqrt (calculo))/(2*a);
+			x2 = (-b -Math.sqrt (calculo))/(2*a);
+			System.out.println(x1 + " y " + x2);
 		}
 		
 		//cierre de Scanner
