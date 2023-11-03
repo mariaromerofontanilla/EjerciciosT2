78d065b0cd48c01bc751f3b7dfee0a07b383a279
mariaromerofontanilla <145675441+mariaromerofontanilla@users.noreply.github.com>
2023-11-03 14:21:01 +0100

    t2MariaRomeroFontanilla

--- EjerciciosT2MaríaRomeroFontanilla/src/condicionales/ejercicio03.java
+++ EjerciciosT2MaríaRomeroFontanilla/src/condicionales/ejercicio03.java
@@ -6,21 +6,27 @@ public class ejercicio03 {
 
 	public static void main(String[] args) {
 		//variable
-		double cantComidaComp, klComida, racion;
-		int numAnimales;
-		double comidarestatnte;
-		
+		double cant= 0, klC= 0, racion;
+		int numA= 0;
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Introduce la cantidad de comida que has comprado: ");
-		cantComidaComp = sc.nextDouble();
+		cant = sc.nextDouble();
 		
 		System.out.println("Introduce cuantos animales tienes: ");
-		numAnimales = sc.nextInt();
+		numA = sc.nextInt();
 		
-		comidarestatnte = cantComidaComp - klComida;
-		racion = comidarestante / numAnimales;
-
+		System.out.println("Introduce el kilo de comida que come cada animal: ");
+		klC = sc.nextDouble();
+		
+		if (cant >= numA) {
+			System.out.println("Si ahi comida suficiente");
+		} else {
+			racion= klC * (cant/numA);
+			System.out.println("La cantidad para cada uso es de : " + racion);
+			
+		}
+		sc.close();
 	}
 
 }
--- EjerciciosT2MaríaRomeroFontanilla/src/condicionales/ejercicio04.java
+++ EjerciciosT2MaríaRomeroFontanilla/src/condicionales/ejercicio04.java
@@ -6,39 +6,91 @@ public class ejercicio04 {
 
 	public static void main(String[] args) {
 		//variable
-		int num;
-		int unidades=0, decenas=0;
+		int num, decena, unidad;
+		String uni = null, deci = null;
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Introduzca un numero del 1 al 99: ");
-		num=sc.nextInt();
+		System.out.println("Introduzca un numero: ");
+		num= sc.nextInt();
 		
-		unidades = num % 10;
-		decenas = num / 10;
+		decena = num/10;
+		unidad = num%10;
 		
-		if (num >= 1 && num <= 99) {
-			switch (num) {
-			case 1: unidades = "uno";
+		if (num >= 1 && num <= 99){
+			switch (unidad) {
+			case 1: uni= "uno";
 			break;
-			
-			case 2: unidades = "dos";
+			case 2: uni= "dos";
 			break;
-			
-			case 3: unidades = "tres";
+			case 3: uni= "tres";
 			break;
-			
-			case 4: unidades = "cuatro";
+			case 4: uni= "cuatro";
 			break;
-			
-			case 5: unidades = "cinco";
+			case 5: uni= "cinco";
 			break;
-			
-			case 6: unidades = "seis";
+			case 6: uni= "seis";
 			break;
-			
-			}
-				
-		}
+			case 7: uni= "siete";
+			break;
+			case 8: uni= "ocho";
+			break;
+			case 9: uni= "nueve";
+			break;
+			default: uni =" ";
+			break;
+			};
+			switch (decena) {
+			case 1: switch (unidad) {
+					case 0: uni= "diez";
+					break;
+					case 1: uni= "once";
+					break;
+					case 2: uni= "doce";
+					break;
+					case 3: uni= "trece";
+					break;
+					case 4: uni= "catorce";
+					break;
+					case 5: uni= "quince";
+					break;
+					default: deci= "dieci";
+					break;
+			};
+			break;
+			case 2: switch (unidad) {
+				case 0: deci= "veinte";
+				break;
+				default: deci= "veinti";
+				break;
+			};
+			break;
+			case 3: deci = "treinta";
+			break;
+			case 4: deci = "cuarenta";
+			break;
+			case 5: deci = "cincuenta";
+			break;
+			case 6: deci = "sesenta";
+			break;
+			case 7: deci = "setenta";
+			break;
+			case 8: deci = "ochenta";
+			break;
+			case 9: deci = "noventa";
+			break;
+			};
 	}
-
+		if(num < 16) {
+			System.out.println(uni);
+		}else if (num > 15 && num < 20) {
+			System.out.println(deci+uni);
+		}else if(num==20) {
+			System.out.println(deci);
+		}else if (num==21){
+			System.out.println(deci+uni);
+		}else {
+			System.out.println(deci + " y " + uni);
+		}
+		sc.close();
+}
 }
